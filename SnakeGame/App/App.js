ca5dd56b066958972d0c99951cc40ad966cb7364
PatrykMandrak <patrykmandrak123@gmail.com>
2019-06-12 13:04:23 +0200

    Fixed problem with rendering snake

--- SnakeGame/App/App.js
+++ SnakeGame/App/App.js
@@ -2,9 +2,6 @@ let boardModel = new Board(40);
 let boardView = new BoardView();
 let boardController = new BoardController(boardModel, boardView);
 
-let fruitContoller = new FruitController;
-let fruitView = new FruitView;
-
 boardController.createBoard();
 boardController.createWall();
 
@@ -12,5 +9,11 @@ let snakeModel = new Snake(11);
 let snakeView = new SnakeView();
 let snakeController = new SnakeController(snakeModel, snakeView, boardModel);
 
+
 snakeController.initializeSnake();
+
+let fruitView = new FruitView;
+let fruit = new Fruit(10);
+let fruitContoller = new FruitController(fruit, fruitView);
+
 fruitContoller.setFruitOdBoard();
--- SnakeGame/controllers/FruitController.js
+++ SnakeGame/controllers/FruitController.js
@@ -1,11 +1,11 @@
 class FruitController {
     constructor(fruitModel, fruitView) {
-        this.fruitView = FruitView;
-        //this.fruitModel = FruitModel;
+        this.fruitView = fruitView;
+        this.fruitModel = fruitModel;
     }
 
     setFruitOdBoard(){
-        setInterval(fruitView.drawFruit(), 2000);
+        this.fruitView.drawFruit();
     }
 
 }
